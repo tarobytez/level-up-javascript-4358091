5ea4d23ee1967c5781df108d82ea316c80e8ae17
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-16 19:53:13 +0000

    Completes challenge 5

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,12 @@
-// Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+// The square brackets make it a sy
+const user = {
+  [username]: "johndoe",
+  [password]: "12345",
+  age: 27
+};
+
+console.log(user.username);
+console.log(user.password);
