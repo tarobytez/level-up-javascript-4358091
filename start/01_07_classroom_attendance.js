25c369301b210c971412dd3d5c36f8ac6b219945
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-16 20:47:30 +0000

    Completes challenge 7

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,24 @@
-// Write your code here
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList} = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ... students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+
+  return students;
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: true,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}))
+
+console.log(
+  getStudents({
+    hasTeachingAssistant: false,
+    classList: ["Michael", "Todd", "Emma", "Sara", "Trey", "Pamela"],
+  }),
+);
