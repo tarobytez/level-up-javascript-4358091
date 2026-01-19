9cc1432ee1bd5ac2d5c871f0064f021c103b6c09
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-19 21:23:41 +0000

    Completes challenge 14

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,8 @@
-// Write your code here
+function urlify(name) {
+  const punctuation = /[.,/#!$%^&*;:{}=!\-?_`~()'"]/g;
+  name = name.replace(punctuation, "");
+
+  return name.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+console.log(urlify("Taro's blog, kinda, maybe? Who knows!"));
