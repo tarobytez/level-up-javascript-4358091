888033b85ae33c89d64c457531c993793133e95a
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-19 19:31:41 +0000

    Completes challenge 12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,15 +1,23 @@
-function numPermutations(letters) {
-  let total;
-  for(let i = 1; i <= letters.length; i++) {
-    if(total === undefined) {
-      total = 1;
-      continue;
-    }
+// function numPermutations(letters) {
+//   let total;
+//   for(let i = 1; i <= letters.length; i++) {
+//     if(total === undefined) {
+//       total = 1;
+//       continue;
+//     }
 
-    total *= i;
+//     total *= i;
+//   }
+
+//   return total;
+// }
+
+function numPermutations(letters) {
+  if(letters.length === 1) {
+    return 1;
   }
 
-  return total;
+  return letters.length * numPermutations(letters.slice(1));
 }
 
-console.log(numPermutations("hello"));
+console.log(numPermutations("daniel"));
