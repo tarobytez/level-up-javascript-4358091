35baf774ad9073a923fbb1d6308d9bb5ac8dcec5
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-19 21:54:51 +0000

    Completes challenge 15

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,19 @@
-// Write your code here
+function checkPassword(password) {
+  // Lookahead "x(?=y) matches "x" if "x" is followed by "y".", essentially an if true i think? Then . mean "matches any single character except for a newline". + means "Matches at least one", and [a-z] means "Matches all characters in the range from a to z"
+  let oneLowerCase = /(?=.+[a-z])/;
+  let oneUpperCase = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let leastEight = /(?=.{8,})/;
+
+  let isValid = 
+    oneLowerCase.test(password) &&
+    oneUpperCase.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecialCharacter.test(password) &&
+    leastEight.test(password);
+
+  return isValid ? "Your password is valid." : "Your password is invalid."
+}
+
+console.log(checkPassword("Th1sIs4G00dP4ssw0rd"));
