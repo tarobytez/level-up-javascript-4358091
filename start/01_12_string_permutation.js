a4a71f39e85e18fc761df4ea6020d4b4a6d8c2fe
taro <57637274+tarobytez@users.noreply.github.com>
2026-01-19 19:27:52 +0000

    Completes 12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,15 @@
-// Write your code here
+function numPermutations(letters) {
+  let total;
+  for(let i = 1; i <= letters.length; i++) {
+    if(total === undefined) {
+      total = 1;
+      continue;
+    }
+
+    total *= i;
+  }
+
+  return total;
+}
+
+console.log(numPermutations("hello"));
